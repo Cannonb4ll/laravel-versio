73f83ea94c8fb06260001d338c0b9b56b3feec31
Dennis Smink <dennissmink@gmail.com>
2016-05-16 18:32:34 +0200

    Readme updates, Domain class progress

--- README.md
+++ README.md
@@ -2,7 +2,13 @@
 
 Laravel Versio wrapper for their API. This package will convert all returned data in nice collections and suitable working data.
 
-# Usage
+## Installation
+
+```
+composer require cannonb4ll/laravel-versio
+```
+
+## Usage
 
 Set up the following variables in your .env file:
 
@@ -15,20 +21,41 @@ Then you can use the class like this:
 
 ```
 $versio = new Versio;
+```
 
-$versio->domains()->listActive(); // List all active domains
-$versio->domains()->listInactive(); // List all inactive domains
-$versio->domains()->listSingle('domain', 'tld'); // List a single domain
-$versio->domains()->isFree('domain', 'tld'); // List a free domain
+## Commands:
+
+```
+- $versio->domains()->listActive(); // List active domains
+- $versio->domains()->listInactive(); // List inactive domains
+- $versio->domains()->listSingle('domain', 'tld'); // List a single domain
+- $versio->domains()->isFree('domain', 'tld'); // Check if domain is free
+- $versio->domains()->updateNameservers('domain', 'tld', $array); // Updates domain nameservers, array structure:
+$nameservers = [
+    'ns1' => 'ns1.website.com',
+    'ns2' => 'ns2.website.com'
+];
+
+- $versio->domains()->dnsRecords('domain', 'tld'); // List all DNS records
+- $versio->domains()->storeDnsRecord('domain', 'tld'); // Store a new DNS record, returns record id, array structure:
+[
+    'name' => 'test',
+    'type' => 'A',
+    'value' => '123.123.123.123',
+    'prio' => 10,
+    'ttl' => 300
+]
+- $versio->domains()->destroyDnsRecord('domain', 'tld', $record_id); // Destroy a DNS record, pass record ID as 3rth argument
+- $versio->domains()->switchDns('domain', 'tld', true/false); // Switch DNS management/nameserver management, 3rth argument bool
 ```
 
 More commands are under progress.
 
-# IMPORTANT
+## IMPORTANT
 
 Please do not use this package yet in a production enviroment, it has not been finished yet and is bound to change abit.
 
-# TODO:
+## TODO:
 
 - Expand with more commands
 - Error handling
--- src/LaravelVersio/Modules/Domain.php
+++ src/LaravelVersio/Modules/Domain.php
@@ -4,8 +4,8 @@ namespace LaravelVersio\Modules;
 
 use LaravelVersio\Core;
 
-class Domain extends Core{
-
+class Domain extends Core
+{
     /**
      * List active domains
      *
@@ -37,10 +37,9 @@ class Domain extends Core{
     /**
      * List a single domain
      *
-     * @param null $domain
-     * @param null $tld
+     * @param string $domain
+     * @param string $tld
      * @return mixed
-     * @throws \Exception
      */
     public function listSingle($domain = null, $tld = null)
     {
@@ -57,10 +56,9 @@ class Domain extends Core{
     /**
      * Check if domain is free
      *
-     * @param null $domain
-     * @param null $tld
+     * @param string $domain
+     * @param string $tld
      * @return array|Collection
-     * @throws \Exception
      */
     public function isFree($domain = null, $tld = null)
     {
@@ -74,14 +72,22 @@ class Domain extends Core{
         return $send;
     }
 
-    public function setNameservers($domain = null, $tld = null, $nameservers = [])
+    /**
+     * Update nameservers for a domain
+     *
+     * @param string $domain
+     * @param string $tld
+     * @param array $nameservers
+     * @return array|\Illuminate\Support\Collection
+     */
+    public function updateNameservers($domain = null, $tld = null, array $nameservers = [])
     {
         $this->command = 'DomainsCheckAvailability';
 
         $this->options['domain'] = $domain;
         $this->options['tld'] = $tld;
 
-        foreach($nameservers as $key => $nameserver){
+        foreach ($nameservers as $key => $nameserver) {
             $this->options[$key] = $nameserver;
         }
 
@@ -93,10 +99,9 @@ class Domain extends Core{
     /**
      * List DNS records for domain
      *
-     * @param null $domain
-     * @param null $tld
+     * @param string $domain
+     * @param string $tld
      * @return static
-     * @throws \Exception
      */
     public function dnsRecords($domain = null, $tld = null)
     {
@@ -111,19 +116,64 @@ class Domain extends Core{
     }
 
     /**
+     * Add a DNS record
+     *
+     * @param string $domain
+     * @param string $tld
+     * @param array $data
+     * @return string record_id
+     */
+    public function storeDnsRecord($domain, $tld, array $data = [])
+    {
+        $this->command = 'DomainsDNSAddRecord';
+
+        $this->options['domain'] = $domain;
+        $this->options['tld'] = $tld;
+        $this->options['name'] = $data['name'];
+        $this->options['type'] = $data['type'];
+        $this->options['value'] = $data['value'];
+        $this->options['prio'] = $data['prio'];
+        $this->options['ttl'] = $data['ttl'];
+
+        $send = $this->send();
+
+        return $send->get('record_id');
+    }
+
+    /**
+     * Destroy a DNS record
+     *
+     * @param string $domain
+     * @param string $tld
+     * @param integer $record_id
+     * @return array|\Illuminate\Support\Collection
+     */
+    public function destroyDnsRecord($domain, $tld, $record_id)
+    {
+        $this->command = 'DomainsDNSDeleteRecord';
+
+        $this->options['domain'] = $domain;
+        $this->options['tld'] = $tld;
+        $this->options['id'] = $record_id;
+
+        $send = $this->send();
+
+        return $send;
+    }
+
+    /**
      * Switch DNS on/off
      *
-     * @param null $domain
-     * @param null $tld
+     * @param string $domain
+     * @param string $tld
      * @param bool $state TRUE = DNS on | FALSE = DNS OFF
      * @return array|Collection
-     * @throws \Exception
      */
-    public function switchDns($domain = null, $tld = null, $state = false)
+    public function switchDns($domain = null, $tld = null, bool $state = false)
     {
-        if($state) {
+        if ($state) {
             $this->command = 'DomainsDNSOn';
-        }else{
+        } else {
             $this->command = 'DomainsDNSOff';
         }
 
